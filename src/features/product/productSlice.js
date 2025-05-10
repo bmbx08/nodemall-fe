9aed34d6b12dd7af4ae6448b3ef3619b6d144bb1
bmbx08 <bmbx08@bu.ac.kr>
2025-05-11 00:45:11 +0900

    added get products logic

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,40 +1,52 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
 import api from "../../utils/api";
-import { showToastMessage } from "../common/uiSlice";
+import {showToastMessage} from "../common/uiSlice";
 
 // 비동기 액션 생성
 export const getProductList = createAsyncThunk(
   "products/getProductList",
-  async (query, { rejectWithValue }) => {}
+  async (query, {rejectWithValue}) => {
+    try {
+      const response = await api.get("/product", {params: {...query}});
+      if (response.status !== 200) throw new Error(response.error);
+      console.log("productList responsee", response);
+
+      return response.data.data;
+    } catch (error) {
+      rejectWithValue(error.error);
+    }
+  }
 );
 
 export const getProductDetail = createAsyncThunk(
   "products/getProductDetail",
-  async (id, { rejectWithValue }) => {}
+  async (id, {rejectWithValue}) => {}
 );
 
 export const createProduct = createAsyncThunk(
   "products/createProduct",
-  async (formData, { dispatch, rejectWithValue }) => {
-    try{
-      const response = await api.post("/product",formData);
-      if(response.status!==200) throw new Error(response.error)
-      dispatch(showToastMessage({message:"상품 생성 완료",status:"success"}))
+  async (formData, {dispatch, rejectWithValue}) => {
+    try {
+      const response = await api.post("/product", formData);
+      if (response.status !== 200) throw new Error(response.error);
+      dispatch(
+        showToastMessage({message: "상품 생성 완료", status: "success"})
+      );
       return response.data.data;
-    }catch(error){
-      return rejectWithValue(error.error)
+    } catch (error) {
+      return rejectWithValue(error.error);
     }
   }
 );
 
 export const deleteProduct = createAsyncThunk(
   "products/deleteProduct",
-  async (id, { dispatch, rejectWithValue }) => {}
+  async (id, {dispatch, rejectWithValue}) => {}
 );
 
 export const editProduct = createAsyncThunk(
   "products/editProduct",
-  async ({ id, ...formData }, { dispatch, rejectWithValue }) => {}
+  async ({id, ...formData}, {dispatch, rejectWithValue}) => {}
 );
 
 // 슬라이스 생성
@@ -61,23 +73,35 @@ const productSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(createProduct.pending,(state,action)=>{
-      state.loading = true;
-    })
-    .addCase(createProduct.fulfilled,(state,action)=>{
-      state.loading=false;
-      state.error="";
-      state.success=true; //상품 생성을 성공했다? -> Dialog 닫음, 실패?->실패 메세지를 Dialog에 보여주고 닫지 않음
-
-    })
-    .addCase(createProduct.rejected,(state,action)=>{
-      state.loading=false;
-      state.error=action.payload;
-      state.success=false;
-    })
+    builder
+      .addCase(createProduct.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(createProduct.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = "";
+        state.success = true; //상품 생성을 성공했다? -> Dialog 닫음, 실패?->실패 메세지를 Dialog에 보여주고 닫지 않음
+      })
+      .addCase(createProduct.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.success = false;
+      })
+      .addCase(getProductList.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(getProductList.fulfilled, (state, action) => {
+        state.loading = false;
+        state.productList = action.payload;
+        state.error = "";
+      })
+      .addCase(getProductList.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
   },
 });
 
-export const { setSelectedProduct, setFilteredList, clearError } =
+export const {setSelectedProduct, setFilteredList, clearError} =
   productSlice.actions;
 export default productSlice.reducer;
--- src/page/AdminProductPage/AdminProductPage.js
+++ src/page/AdminProductPage/AdminProductPage.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { Container, Button } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import { useSearchParams, useNavigate } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Container, Button} from "react-bootstrap";
+import {useDispatch, useSelector} from "react-redux";
+import {useSearchParams, useNavigate} from "react-router-dom";
 import ReactPaginate from "react-paginate";
 import SearchBox from "../../common/component/SearchBox";
 import NewItemDialog from "./component/NewItemDialog";
@@ -16,7 +16,7 @@ const AdminProductPage = () => {
   const navigate = useNavigate();
   const [query] = useSearchParams();
   const dispatch = useDispatch();
-  const { productList, totalPageNum } = useSelector((state) => state.product);
+  const {productList, totalPageNum} = useSelector((state) => state.product);
   const [showDialog, setShowDialog] = useState(false);
   const [searchQuery, setSearchQuery] = useState({
     page: query.get("page") || 1,
@@ -37,9 +37,20 @@ const AdminProductPage = () => {
   ];
 
   //상품리스트 가져오기 (url쿼리 맞춰서)
+  useEffect(() => {
+    dispatch(getProductList({...searchQuery}));
+  }, [query]);
 
   useEffect(() => {
     //검색어나 페이지가 바뀌면 url바꿔주기 (검색어또는 페이지가 바뀜 => url 바꿔줌=> url쿼리 읽어옴=> 이 쿼리값 맞춰서  상품리스트 가져오기)
+    if(searchQuery.name===""){
+      delete searchQuery.name;
+    }
+    console.log("search query",searchQuery)
+    const params = new URLSearchParams(searchQuery) //URLSearchParams가 객체 형태인 searchQuery를 query형태로 바꿔줌
+    const query = params.toString(); //하지만 string 형태로 변환해야지 사용가능
+    console.log("queryy", query);
+    navigate("?"+query)
   }, [searchQuery]);
 
   const deleteItem = (id) => {
@@ -58,9 +69,12 @@ const AdminProductPage = () => {
     setShowDialog(true);
   };
 
-  const handlePageClick = ({ selected }) => {
+  const handlePageClick = ({selected}) => {
     //  쿼리에 페이지값 바꿔주기
   };
+
+  //searchbox에서 검색어를 읽어온다 =>  엔터를 치면 => searchQuery객체가 업데이트가 됨 {name: 스트레이트 팬츠}
+  //=>searchQuery객체 안에 아이템 기준으로 url을 새로 생성해서 호출 &name=스트레이트+팬츠 => url쿼리 읽어오기 => url쿼리 기준으로 BE에 검색조건과함께 호출한다.
 
   return (
     <div className="locate-center">
@@ -79,7 +93,7 @@ const AdminProductPage = () => {
 
         <ProductTable
           header={tableHeader}
-          data=""
+          data={productList}
           deleteItem={deleteItem}
           openEditForm={openEditForm}
         />
